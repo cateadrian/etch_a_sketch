76ab6e956a5bd0d29ca05594df6236f6153a39d5
Adrian Cate <77905982+cateadrian@users.noreply.github.com>
2024-06-14 20:06:13 -0600

    Add draw feature

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ for(let i = 1; i < 257; i++){
     const div = document.createElement("div");
     div.setAttribute("class","div");
     container.appendChild(div);
+    div.addEventListener("mouseenter", function(e){
+        let target = e.target;
+
+        target.style.backgroundColor = "black";
+
+    });
 };
+
