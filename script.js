0afec25ff6b11d3c4758dc898cd996499eb70081
Adrian Cate <77905982+cateadrian@users.noreply.github.com>
2024-06-16 12:32:12 -0600

    Reworked makeGrid function to accept different grid sizes

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let container = document.querySelector("#container");
 
-for(let i = 1; i < 257; i++){
-    const div = document.createElement("div");
-    div.setAttribute("class","div");
-    container.appendChild(div);
-    div.addEventListener("mouseenter", function(e){
-        let target = e.target;
+function makeGrid(n){ 
+    for(let i = 1; i < (n*n) + 1; i++){
+        const div = document.createElement("div");
+        div.setAttribute("class","div");
+        div.style.flex = `0 0 ${100/(n)}%`;
+        container.appendChild(div);
 
-        target.style.backgroundColor = "black";
+        div.addEventListener("mouseenter", function(e){
+             let target = e.target;
 
-    });
-};
+             target.style.backgroundColor = "black";
+         });
+    };
+}
+
+// Having more than 50 makes the computer really slow
+// Initializes a grid 16x16
+makeGrid(16);
+
 
